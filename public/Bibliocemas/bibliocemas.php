c6531d071f6acf3252e7daf1888d8a5c0000a825
Cristofep <srcristofep@gmail.com>
2024-04-16 09:03:19 -0400

    Landig page bibliocemas

---
+++ public/Bibliocemas/bibliocemas.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliocemas</title>
+    <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/css/bibliocemas/bibliocemas.css">
+    <link rel="stylesheet" href="../assets/css/fonst.css">
+    <link rel="stylesheet" href="../assets/css/scrollbar.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="menu">
+            <img src="../assets/img/logo.png" alt="">
+            <ul class="menu_list">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Libros</a>
+                </li>
+                <li>
+                    <a href="#alquilar">Alquielar</a>
+                </li>
+                <li>
+                    <a href="#">Uniformes</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="bibliocemas">
+            <p>Bibliocemas</p>
+        </div>
+        <div class="book_container">
+            <div class="books">
+                <img class="book_portada" src="https://grajedamena.ufm.edu/wp-content/uploads/2019/05/el-senor-presidente_rjdqtn.jpg" alt="#">
+                <p class="book_titulo">Titulo</p>
+            </div>
+            <div class="books">
+                <img class="book_portada" src="https://grajedamena.ufm.edu/wp-content/uploads/2019/05/el-senor-presidente_rjdqtn.jpg" alt="#">
+                <p class="book_titulo">Titulo</p>
+            </div>
+            <div class="books">
+                <img class="book_portada" src="https://grajedamena.ufm.edu/wp-content/uploads/2019/05/el-senor-presidente_rjdqtn.jpg" alt="#">
+                <p class="book_titulo">Titulo</p>
+            </div>
+            <div class="books">
+                <img class="book_portada" src="https://grajedamena.ufm.edu/wp-content/uploads/2019/05/el-senor-presidente_rjdqtn.jpg" alt="#">
+                <p class="book_titulo">Titulo</p>
+            </div>
+            <div class="books">
+                <img class="book_portada" src="https://grajedamena.ufm.edu/wp-content/uploads/2019/05/el-senor-presidente_rjdqtn.jpg" alt="#">
+                <p class="book_titulo">Titulo</p>
+            </div>
+            <div class="books">
+                <img class="book_portada" src="https://grajedamena.ufm.edu/wp-content/uploads/2019/05/el-senor-presidente_rjdqtn.jpg" alt="#">
+                <p class="book_titulo">Titulo</p>
+            </div>
+        </div>
+
+        <div id="alquilar">
+            <div></div>
+            <div>
+                <form action="" method="post">
+                    <label for="">nombre completo</label>
+                    <input type="text" name="" id="">
+                    <label for="">correo Electronico</label>
+                    <input type="email" name="" id="">
+                    <label for="">curso</label>
+                    <input type="text" name="" id="">
+                    <label for="">nombre del libro</label>
+                    <input type="text" name="" id="">
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="./assets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/css/fonst.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="./Secretaria/secretaria.php">secretaria</a>
+            </li>
+            <li>
+                <a href="./Bibliocemas/bibliocemas.php">bibliocemas</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
