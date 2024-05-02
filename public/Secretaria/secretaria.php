e5ae68c9a16124ceffa644bc621bbf02b615d085
Cristofep <srcristofep@gmail.com>
2024-05-02 08:34:54 -0400

    Iniciando web Secretaria
    Signed-off-by: Cristofep <srcristofep@gmail.com>

---
+++ public/Secretaria/secretaria.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secretaria</title>
+    <link rel="shortcut icon" href="../../assets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/css/secretaria/secretaria.css">
+    <link rel="stylesheet" href="../../assets/css/secretaria/menu.css">
+
+    <link rel="stylesheet" href="../../assets/css/fonst.css">
+    <link rel="stylesheet" href="../../assets/css/scrollbar.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="menu">
+            <img src="../../assets/img/logo.png" alt="">
+            <ul class="menu_list">
+                <li>
+                    <a href="../index.php">Home</a>
+                </li>
+                <li>
+                    <a href="#">#</a>
+                </li>
+                <li>
+                    <a href="#">#</a>
+                </li>
+                <li>
+                    <a href="#">#</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
