82b428625be805da18cb19d211d9ff543b2aeeeb
Alexanderkaizen <alexanderespinalkaizen@gmail.com>
2024-05-06 12:16:28 -0400

    actualizacion menu biblioteca

--- public/Bibliocemas/bibliocemas.php
+++ public/Bibliocemas/bibliocemas.php
@@ -13,12 +13,16 @@
 
     <link rel="stylesheet" href="../../assets/css/fonst.css">
     <link rel="stylesheet" href="../../assets/css/scrollbar.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <header class="header">
         <nav class="menu">
-            <h1 class="logo_txt">Bibliocemas</h1>
+            <h1 class="logo">Bibliocemas</h1>
             <ul class="menu_list">
                 <li>
                     <a href="../index.php">Inicio</a>
@@ -30,7 +34,7 @@
                     <a href="./Libros/alquilar.html">Alquilar</a>
                 </li>
                 <li>
-                <a href="./contact_us.html">Contact Us</a>
+                    <a class="contact" href="./contact_us.html">Contact Us</a>
                 </li>
             </ul>
         </nav>
@@ -38,7 +42,7 @@
     <main>
 
         <div class="biblioteca">
-            <img class="biblioteca_img" src="../../assets/img/biblioteca.jpeg" alt="">
+           <div class="biblioteca_img"></div>
             <p>Bibliocemas</p>
         </div>
 
@@ -64,13 +68,16 @@
                     <div class="footer-col">
                         <h4>Siguenos</h4>
                         <div class="social-links">
-                            <a href="https://www.facebook.com/p/Centro-Educativo-Manuel-Acevedo-Serrano-F-y-A-100063508002864/?locale=es_LA" target="_blank"><img src="../../assets/img/svg/facebook.svg" alt=""></a>
-                            <a href="https://www.instagram.com/cemas_1963/" target="_blank"><img src="../../assets/img/svg/instagram.svg" alt=""></a>
+                            <a href="https://www.facebook.com/p/Centro-Educativo-Manuel-Acevedo-Serrano-F-y-A-100063508002864/?locale=es_LA"
+                                target="_blank"><img src="../../assets/img/svg/facebook.svg" alt=""></a>
+                            <a href="https://www.instagram.com/cemas_1963/" target="_blank"><img
+                                    src="../../assets/img/svg/instagram.svg" alt=""></a>
                         </div>
                     </div>
                 </div>
             </div>
         </footer>
-    </body>
-    <script type="module" src="../../assets/js/mostarBooks.js"></script>
+</body>
+<script type="module" src="../../assets/js/mostarBooks.js"></script>
+
 </html>
